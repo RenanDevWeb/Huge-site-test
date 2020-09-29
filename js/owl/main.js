6ef4952f54b04ecff8259672ccfba971171c1451
Renan <renansalmazio@gmail.com>
2020-09-29 00:08:37 -0300

    adicionando final ao site

---
+++ js/owl/main.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin: 10,
+    padding:0,
+    center: true,
+    nav:false,
+    
+    responsive:{
+        0:{
+            items:6
+        },
+        600:{
+            items:6
+        },
+        1000:{
+            items:6
+        }
+    }
+})
